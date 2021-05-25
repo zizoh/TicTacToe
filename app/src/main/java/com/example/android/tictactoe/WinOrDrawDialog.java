40567ebc88ceed40a6079c8085a10afa82e38fd8
zizoh <zizoh.anto@getir.com>
2021-05-25 20:28:22 +0100

    Update dependencies and release v1.0.0

--- app/src/main/java/com/example/android/tictactoe/WinOrDrawDialog.java
+++ app/src/main/java/com/example/android/tictactoe/WinOrDrawDialog.java
@@ -2,8 +2,8 @@ package com.example.android.tictactoe;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.appcompat.app.AlertDialog;
 import android.view.ContextThemeWrapper;
 
 public class WinOrDrawDialog extends DialogFragment {
